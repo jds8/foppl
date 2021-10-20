04af65a27f619e2340f8d59a3cac384ef553226c
Justice Sefas <jsefas@cs.ubc.ca>
2021-10-19 21:26:59 -0700

    graph tests

--- src/foppl/graph_based_sampling.clj
+++ src/foppl/graph_based_sampling.clj
@@ -5,14 +5,8 @@
 (require '[clojure.java.shell :as shell :refer [sh]])
 (use '[clojure.java.shell :only [sh]])
 
-(defn cwd [] (let [pwd ((sh "pwd") :out)] (str (.substring pwd 0 (- (count pwd) 1)) "/")))
 (def all-records (json/read-str (slurp (str (cwd) "../CS532-HW2/graph_test_2.json"))
                 :key-fn str))
-
-(defn graph [i]
-  (json/read-str ((shell/with-sh-dir
-    (str (cwd) "../daphne")
-    (sh "lein" "run" "-f" "json" "graph" "-i" (str "../CS532-HW2/programs/tests/deterministic/test_" i ".daphne"))) :out)))
 
 (defn deterministic-eval [exp]
   (match [exp]
@@ -68,14 +62,27 @@
     return
   ))
 
-(def all-records1 (graph 1))
-(def all-records2 (graph 2))
-(def all-records3 (graph 3))
-(def all-records4 (graph 4))
-(def all-records6 (graph 7))
-(sample-from-joint all-records1)
-(sample-from-joint all-records2)
-(sample-from-joint all-records3)
-(sample-from-joint all-records4)
+(defn diff [ret truth]
+  (if (= (type truth) clojure.lang.PersistentVector)
+    (reduce + (map - ret truth))
+    (- ret truth)))
 
-(top-sort-graph all-records1)
+(defn cwd [] (let [pwd ((sh "pwd") :out)] (str (.substring pwd 0 (- (count pwd) 1)) "/")))
+(defn graph-tests [type i]
+  (json/read-str ((shell/with-sh-dir
+    (str (cwd) "../daphne")
+    (sh "lein" "run" "-f" "json" "desugar" "-i" (str "../CS532-HW2/programs/tests/" type "/test_" i ".daphne"))) :out)))
+
+(defn run-deterministic-tests []
+  (for [i (map (fn [x] (+ 1 x)) (range 13))]
+    (do (println (str "running test " i))
+    (let [ast (graph-tests "deterministic" i)
+          truth (load-truth i)
+          [ret sig] (evaluate-program ast)]
+            (if (> (abs (diff truth ret)) 0.001)
+              (let [msg (str "return value " ret " is not equal to truth " truth " for exp " ast)]
+                    (do (print msg) msg)
+              (do (print "passed test " i) (str "passed test " i))))))
+  )
+
+(run-deterministic-tests)
